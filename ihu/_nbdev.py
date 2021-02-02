9675c377e045b32a4e2c158754ad18d65074b0fc
TejasAvinashShetty <tejasshetty.1808@gmail.com>
2021-02-02 10:48:02 +0530

    nbdev everywhere

--- ihu/_nbdev.py
+++ ihu/_nbdev.py
@@ -15,14 +15,13 @@ index = {"ihu": "00_core.ipynb",
          "evolution_time": "04_tests.ipynb",
          "no_time_step_for_eval": "04_tests.ipynb",
          "identity_ham": "04_tests.ipynb",
-         "silly_ham": "04_tests.ipynb",
-         "H0": "04_tests.ipynb",
-         "H1": "04_tests.ipynb",
          "H": "04_tests.ipynb",
          "psi0": "04_tests.ipynb",
          "times": "04_tests.ipynb",
          "mere_result": "04_tests.ipynb",
-         "q_ham": "04_tests.ipynb"}
+         "q_ham": "04_tests.ipynb",
+         "psi0_ndarray": "04_tests.ipynb",
+         "upsi": "04_tests.ipynb"}
 
 modules = ["core.py",
            "reshape.py",
